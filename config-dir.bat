bdae9fd1e6b164f1ebcf4bafea20690e94f6c81d
memory device <mem.device@gmail.com>
2021-01-27 17:48:17 +0100

    Update config-dir.bat

--- config-dir.bat
+++ config-dir.bat
@@ -1,1 +1,14 @@
-zy
+mkdir c:\emacs
+mkdir c:\cygwin64
+mkdir c:\lm
+mkdir c:\lm\software
+mkdir d:\home
+mkdir d:\home\.backup\emacs
+mkdir d:\home\.emacs.d
+mkdir d:\home\.emacs.d\lisp
+mkdir d:\home\.emacs.d\themes
+mkdir d:\home\.emacs.d\bat
+
+
+
+
