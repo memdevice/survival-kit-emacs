a75e046180c6ba219bad73d82598d4842d8a45b0
memory device <mem.device@gmail.com>
2021-02-18 16:28:27 +0100

    Update custom-file.el

--- .emacs.d/custom-file.el
+++ .emacs.d/custom-file.el
@@ -1,15 +1,2 @@
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:family "Consolas" :foundry "outline" :slant normal :weight normal :height 120 :width normal)))))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(column-number-mode t)
- '(cua-mode t nil (cua-base))
- '(custom-enabled-themes '(leuven))
+
  
